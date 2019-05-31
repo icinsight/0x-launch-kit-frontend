89cafe20f724383b28e5b8ed82d9aa159f90ada4
icinsight <39129866+icinsight@users.noreply.github.com>
2019-05-31 13:18:31 -0300

    Added pairs 0xbtc, DAI, WETH, ZRX

--- src/common/markets.ts
+++ src/common/markets.ts
@@ -2,11 +2,31 @@ import { CurrencyPair, TokenSymbol } from '../util/types';
 
 export const availableMarkets: CurrencyPair[] = [
     {
+        base: TokenSymbol.Dai,
+        quote: TokenSymbol.0xbtc,
+    },
+        {
+        base: TokenSymbol.Weth,
+        quote: TokenSymbol.0xbtc,
+    },
+    {
         base: TokenSymbol.Zrx,
+        quote: TokenSymbol.0xbtc,
+    },
+    {
+        base: TokenSymbol.Dai,
         quote: TokenSymbol.Weth,
     },
     {
-        base: TokenSymbol.Mkr,
-        quote: TokenSymbol.Weth,
+        base: TokenSymbol.Dai,
+        quote: TokenSymbol.Zrx,
+    },
+    {
+        base: TokenSymbol.Weth,
+        quote: TokenSymbol.Dai,
+    },
+    {
+        base: TokenSymbol.Weth,
+        quote: TokenSymbol.Zrx,
     },
 ];
